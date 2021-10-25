78e82e27d73bc09c02e4d6635f974886b1e9fb11
grakky <akimgrebenkin21@gmail.com>
2021-10-25 20:53:55 +0300

    comments fix

--- News/subscription.py
+++ News/subscription.py
@@ -1,4 +1,3 @@
-import channels_graphql_ws
 import graphene
 from channels.layers import get_channel_layer
 
@@ -6,18 +5,21 @@ channel_layer = get_channel_layer()
 
 
 class BreakingNewsType(graphene.ObjectType):
+    """Уведомление о срочной новости"""
+
     news_id = graphene.Int()
     news_title = graphene.String()
 
 
 class CommentOnNewsType(graphene.ObjectType):
+    """Обновление комментариев к конкретной новости в live режиме"""
+
     username = graphene.String()
     comment_text = graphene.String()
     news_id = graphene.Int()
 
 
 class NewsSubscription(graphene.ObjectType):
-    """Срабатывает при появлении срочной новости"""
 
     breaking_news = graphene.Field(BreakingNewsType)
     comment_on_news = graphene.Field(CommentOnNewsType, news_id=graphene.Int())
